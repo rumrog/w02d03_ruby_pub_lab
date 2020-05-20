efae90099df10538d322c7fc84ab03ac8663fd5b
Rumen Roger <rumenroger@gmail.com>
2020-05-20 14:45:07 +0100

    FoodTest test_food_name_and_price_and_rejuvenation_level PASS

---
+++ start-point/specs/food_spec.rb
@@ -0,0 +1,16 @@
+require('minitest/autorun')
+require('minitest/reporters')
+require_relative('../food.rb')
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+class FoodTest < MiniTest::Test
+
+    def test_food_name_and_price_and_rejuvenation_level()
+        food = Food.new("Haggis", 10, 4)
+        assert_equal("Haggis", food.name)
+        assert_equal(10, food.price)
+        assert_equal(4, food.rejuvenation_level)
+    end
+
+end
