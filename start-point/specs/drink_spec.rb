32eaa70658ef00df51cbae1e134c2e0c1d7d80c8
Rumen Roger <rumenroger@gmail.com>
2020-05-20 11:37:07 +0100

    DrinkTest test_drink_name_and_price PASS

--- start-point/specs/drink_spec.rb
+++ start-point/specs/drink_spec.rb
@@ -6,4 +6,10 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class DrinkTest < MiniTest::Test
 
+    def test_drink_name_and_price()
+        drink = Drink.new("CocaCola", 2)
+        assert_equal("CocaCola", drink.name)
+        assert_equal(2, drink.price)
+    end
+
 end
